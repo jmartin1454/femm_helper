b11bde44e9729d0747d09885367b35a089d5d2f7
jmartin1454 <j.martin@uwinnipeg.ca>
2016-05-26 12:28:27 -0500

    added a few more examples of how to read from COMSOL files.

--- femm_helper.sh
+++ femm_helper.sh
@@ -3,3 +3,5 @@
 ./femm_helper.py -s -f spherical.fem < femm_helper.in
 ./femm_helper.py -c -f cylindrical.fem < femm_helper_cyl.in
 ./femm_helper.py -c -r -f read.fem < femm_helper_read.in
+# ./femm_helper.py -c -r -f read-100.fem -i Cylinder-100-turns-inner.txt -o Cylinder-100-turns-outer.txt < femm_helper_read.in
+# ./femm_helper.py -c -r -f read-100mA.fem -i Cylinder-Inner-100mA.txt -o Cylinder-outer-100mA.txt < femm_helper_read.in
